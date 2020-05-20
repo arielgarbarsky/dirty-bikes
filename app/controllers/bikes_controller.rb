7fd83de50039fbc655b681f184502bfa4808e740
Schneur Kamensky <szkamens@gmail.com>
2020-05-20 14:32:55 +0300

    added coudinary and function to create bike

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -3,7 +3,25 @@ class BikesController < ApplicationController
     @bike = Bike.new
   end
 
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.user = current_user
+
+    if @bike.save
+      redirect_to @bike, notice: 'Your Dirt Bike was added! 🚵‍♂️'
+    else
+      render :new
+    end
+  end
+
   def show
-    @bike = Bike.new
+    # changed from  Bike.new to Bike.find(params[:id])
+    @bike = Bike.find(params[:id])
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:brand, :price, :location, photos: [])
   end
 end
