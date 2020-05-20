352939565892f2e83e140b445767ca6c3d088ba8
Schneur Kamensky <szkamens@gmail.com>
2020-05-20 22:11:32 +0300

    revised seeds to 15 and corrected desc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@ files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1589977107/p
 files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1589983219/photo-1500578862199-d2872c0781ec_dvdkfa_a02wgn.jpg")
 files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1589982800/photo-1418793124283-7935cb656d02_kiu2ou.jpg")
 files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1589982776/photo-1542550546-88afdd84b64f_vmglrl.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590000851/photo-1518886735984-4d11d9747897_uqunph_jg4ld9.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1589982753/photo-1489730896401-ae2beb188bef_qkho7d.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001097/photo-1515540468442-bcec79e94728_dmnt4s.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001198/photo-1529384342872-c0d18db57a6c_d9ahqr.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001261/photo-1503351392692-acfef9427797_wy47zk.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001413/photo-1509493435975-51bcb821d252_fadhmz.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001462/photo-1549530195-08c51a454504_bhflzb.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001494/photo-1496705048923-c5a338800024_rytz10.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001580/photo-1536346218367-c0b12f24ef7e_krz0ir.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001662/photo-1556176226-75d39eda8fee_j5vfq3.jpg")
+files << URI.open("https://res.cloudinary.com/szkloud/image/upload/v1590001730/photo-1562802963-4a08e54fddf4_m6lism.jpg")
 
 puts 'Cleaning database...'
 Booking.destroy_all
@@ -18,7 +29,7 @@ users = []
 
 users << User.create!(first_name: 'bob', last_name: 'smith', username: 'bob1', email: 'bob@bob.com', password: '123456')
 
- 5.times do
+ 16.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   username = Faker::Internet.username(4)
@@ -36,12 +47,12 @@ puts 'Creating Bikes...'
 
 bikes = []
 
- 5.times do |i|
+ 16.times do |i|
   brand = %w[Honda Fantic KTM Yamaha Suzuki Cyberquad].sample
   location = Faker::Address.city
   price = rand(50..150)
   user = users[i]
-  description = Faker::Lorem.words(18)
+  description = Faker::Lorem.sentence(20)
   bike = Bike.new(brand: brand, location: location, price: price, user: user, description: description)
   bike.photos.attach(io: files[i], filename: "bik#{i}.png", content_type: 'image/png')
   bike.save
@@ -57,7 +68,7 @@ puts 'Creating Bookings...'
 
 bookings = []
 
- 5.times do |i|
+ 16.times do |i|
   rent_date = Faker::Date.forward(60)
   user = users[i]
   bike = bikes[i+1]
