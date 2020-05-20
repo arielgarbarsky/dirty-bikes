4f7b402c68c64e7375bf9f33de1aa23774bc30cb
Naftali Ajnwojner <naftali.ajnwojner@gmail.com>
2020-05-20 15:07:50 +0300

    first layout for dashboard

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -4,4 +4,6 @@ class Bike < ApplicationRecord
 
   validates :brand, inclusion: { in: %w(honda fantic ktm yamaha suzuki cyberquad) }
   validates :user, presence: true
+  validates :price, presence: true
+  validates_numericality_of :price
 end
