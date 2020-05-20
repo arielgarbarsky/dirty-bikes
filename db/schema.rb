cc4603f323c5c2f2490d5640fa3c41cb7e15c8aa
Vincent Katz <vinz.k@live.fr>
2020-05-20 13:45:23 +0300

    created the showpage, image needs to be set up with cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_154221) do
+ActiveRecord::Schema.define(version: 2020_05_20_083431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_154221) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
